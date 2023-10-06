33cce8b2e36cbc24ba067401868b68f3ca68660b
Isaac <teryima127@gmail.com>
2023-10-06 02:12:43 +0100

    Merkle aidrop

---
+++ src/Airdrop.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "solmate/src/utils/MerkleProofLib.sol";
+
+
+contract Airdrop is ERC20 {
+    bytes32 merkleRoot;
+
+    constructor(bytes32 _root) ERC20("Merkle", "MKL") {
+        merkleRoot = _root;
+    }
+
+    mapping(address => bool) hasClaimed;
+    event AddressClaim(address account, uint256 amount);
+
+    function claim(
+        bytes32[] calldata _merkleProof,
+        address claimer,
+        uint256 _amount
+    ) external returns (bool success) {
+        require(!hasClaimed[claimer], "You have already claimed!");
+        bytes32 node = keccak256(abi.encodePacked(claimer, _amount));
+        success = MerkleProofLib.verify(_merkleProof, merkleRoot, node);
+        require(success, "MerkleDistributor: Invalid proof.");
+        hasClaimed[claimer] = true;
+        _mint(claimer, _amount);
+        hasClaimed[claimer] = true;
+        emit AddressClaim(claimer, _amount);
+    }
+}
